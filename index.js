a12f732e32696b0da64b08fece850f9c6860ad9b
Folahan Mosunmola <folahan@umweltdev.com>
2025-09-27 06:02:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
-const app = express();
 const dotenv = require("dotenv").config();
-const PORT = 8080;
+const cors = require("cors");
+
 const userRouter = require("./routes/userRoute");
 const categoryRouter = require("./routes/categoryRoute");
 const brandRouter = require("./routes/brandRoute");
@@ -11,16 +11,35 @@ const productRouter = require("./routes/productRoute");
 const addressRouter = require("./routes/addressRoute");
 const cardRouter = require("./routes/cardRoute");
 const pricingRouter = require("./routes/pricingRoute");
-const cors = require("cors");
 
+const app = express();
+const PORT = process.env.PORT || 8080;
 
- dbConnect();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-}));
+dbConnect();
+
+const allowedOrigins = [
+  "https://groceri-store.netlify.app", 
+  "http://localhost:5173",            
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
+
+// routes
 app.use("/api/user", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/brand", brandRouter);
@@ -29,9 +48,9 @@ app.use("/api/address", addressRouter);
 app.use("/api/card", cardRouter);
 app.use("/api/pricing", pricingRouter);
 
-
-app.use(notFound); 
+app.use(notFound);
 app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log(`Server is running  at PORT ${PORT}`);
+  console.log(`🚀 Server running at port ${PORT}`);
 });
